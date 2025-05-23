9801adecb45a3254b4ea8b579edee6a62585683f
Asqarbek Olimov <88034288+asqarbekolimov@users.noreply.github.com>
2025-05-23 23:49:44 +0500

    Plugin HTML

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = (env) => {
@@ -9,5 +10,11 @@ module.exports = (env) => {
       filename: "[name].[contenthash].bundle.js",
       clean: true,
     },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "public", "index.html"),
+        filename: "index.html",
+      }),
+    ],
   };
 };
