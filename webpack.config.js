913d812099127b7b0dc6f3893fb9925229fe8c71
Asqarbek Olimov <88034288+asqarbekolimov@users.noreply.github.com>
2025-05-23 12:23:42 +0500

    Output

--- webpack.config.js
+++ webpack.config.js
@@ -2,4 +2,9 @@ const path = require("path");
 
 module.exports = {
   entry: path.resolve(__dirname, "src", "main.js"),
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "[name].[contenthash].bundle.js",
+    clean: true,
+  },
 };
